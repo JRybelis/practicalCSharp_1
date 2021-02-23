9ea56ab7dbeb8c1c080f477060cab8d919743d73
JRybelis <72306551+JRybelis@users.noreply.github.com>
2021-02-23 18:57:25 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace FirstProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter your birth year.");
+            string userInput = Console.ReadLine();
+
+            int birthYear = int.Parse(userInput);
+            bool isUserOfLegalAge = DateTime.Now.Date.Year - birthYear > 18;
+
+            if (isUserOfLegalAge)
+            {
+                Console.WriteLine("Hello");
+            }
+            else
+            {
+                Console.WriteLine("Access denied");
+            }
+            Console.WriteLine("Bye");
+        }
+    }
+}
