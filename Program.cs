2df3aac01a06c80f5fb263b1e9164615f2f23d76
JRybelis <72306551+JRybelis@users.noreply.github.com>
2021-02-23 19:20:11 +0200

    Switch statements

--- Program.cs
+++ Program.cs
@@ -14,13 +14,32 @@ namespace FirstProject
 
             if (isUserOfLegalAge)
             {
-                Console.WriteLine("Hello");
+                Console.WriteLine("Please come in.");
+            } else if (DateTime.Now.DayOfWeek == DayOfWeek.Sunday) {
+                Console.WriteLine("Oh well, it's Sunday.");
             }
             else
             {
-                Console.WriteLine("Access denied");
+                Console.WriteLine("Access denied.");
             }
             Console.WriteLine("Bye");
+
+            switch (DateTime.Now.DayOfWeek)
+            {
+                case DayOfWeek.Monday:
+                    Console.WriteLine("It is the start of the week today.");
+                    break;
+                case DayOfWeek.Friday:
+                    Console.WriteLine("It is finally Friday.");
+                    break;
+                case DayOfWeek.Saturday:
+                case DayOfWeek.Sunday:
+                    Console.WriteLine("The weekend has come.");
+                    break;
+                default:
+                    Console.WriteLine("It is still the work week...");
+                    break;
+            }
         }
     }
 }
