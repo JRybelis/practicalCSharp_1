213c4953eff30457344474583f67cd5b0f20461f
JRybelis <72306551+JRybelis@users.noreply.github.com>
2021-02-24 17:38:47 +0200

    time span calculation

---
+++ TimeAlive.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace FirstProject
+{
+    public class TimeAlive
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter the day you were born on:");
+            int day = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Please enter the month you were born in:");
+            int month = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Please enter your year of birth:");
+            int year = int.Parse(Console.ReadLine());
+
+            DateTime dateOfBirth = new DateTime(year, month, day);
+
+            TimeSpan timeSpan = DateTime.Now - dateOfBirth;
+
+            Console.WriteLine("You were born " + timeSpan.TotalDays + "days ago");
+        }
+    }
+}
